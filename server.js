c903db1b576d0aa3706844f0659bce9923d98d2c
JesseIguess <blueberry21007@gmail.com>
2023-04-25 22:36:17 +0000

    .

---
+++ server.js
@@ -0,0 +1,63 @@
+// Import the functions you need from the SDKs you need
+
+// import { initializeApp } from "firebase/app";
+// import { getAnalytics } from "firebase/analytics";
+
+const firebaseAppP = require("firebase/app");
+const firebaseAnalP = require("firebase/analytics");
+const express = require('express');
+const path = require('path');
+const fs = require('fs');
+const http = require('http');
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyCKNQF-XomngqDadWvppboGoDvid3Yob4Q",
+    authDomain: "peeisstoredintheballs3.firebaseapp.com",
+    projectId: "peeisstoredintheballs3",
+    storageBucket: "peeisstoredintheballs3.appspot.com",
+    messagingSenderId: "625981462803",
+    appId: "1:625981462803:web:7ec01d339e9fe84cb262e6",
+    measurementId: "G-QHYLP96DJ8"
+};
+
+// Initialize Firebase
+const firebaseApp = firebaseAppP.initializeApp(firebaseConfig);
+// const firebaseAnal = firebaseAnalP.getAnalytics(firebaseApp);
+
+
+// =================================================
+
+// -- HOSTING STEPS --
+//
+// python -m http.server 8000
+
+
+console.log("server.js succesfully started");
+
+// -- FIREBASE HOSTING STEPS --
+//
+// firebase login
+// firebase init
+// firebase deploy
+
+
+const expressApp = express();
+expressApp.use(express.json());
+
+var pagesRoot = "public";
+var root = path.dirname(require.main.filename);
+
+function GetPublicFile(filename) {
+    return root + "/" + pagesRoot + "/" + filename;
+}
+
+expressApp.get("/testing", (req, res) => {
+    res.sendFile(GetPublicFile("testing.html"));
+});
+
+expressApp.listen(8000);
